462f1085e6559c2d96c8e3e1d9888e2c6948952f
XantreDev <valerik007.vs@gmail.com>
2024-07-28 21:28:38 +0200

    moved some pieces

--- src/lib/create-tailwind-merge.ts
+++ src/lib/create-tailwind-merge.ts
@@ -8,35 +8,6 @@ type CreateConfigSubsequent = (config: GenericConfig) => GenericConfig
 type TailwindMerge = (...classLists: ClassNameValue[]) => string
 type ConfigUtils = ReturnType<typeof createConfigUtils>
 
-export function createTailwindMergeBench(
-    mergeClassList: (value: string, configUtils: ConfigUtils) => string,
-    createConfigFirst: CreateConfigFirst,
-    ...createConfigRest: CreateConfigSubsequent[]
-): TailwindMerge {
-    let configUtils: ConfigUtils
-    let functionToCall = initTailwindMerge
-
-    function initTailwindMerge(classList: string) {
-        const config = createConfigRest.reduce(
-            (previousConfig, createConfigCurrent) => createConfigCurrent(previousConfig),
-            createConfigFirst() as GenericConfig,
-        )
-
-        configUtils = createConfigUtils(config)
-        functionToCall = tailwindMerge
-
-        return tailwindMerge(classList)
-    }
-
-    function tailwindMerge(classList: string) {
-        return mergeClassList(classList, configUtils)
-    }
-
-    return function callTailwindMerge() {
-        return functionToCall(twJoin.apply(null, arguments as any))
-    }
-}
-
 export function createTailwindMerge(
     createConfigFirst: CreateConfigFirst,
     ...createConfigRest: CreateConfigSubsequent[]
@@ -68,88 +39,6 @@ export function createTailwindMerge(
         // }
 
         const result = mergeClassList(classList, configUtils)
-        // cacheSet(classList, result)
-
-        return result
-    }
-
-    return function callTailwindMerge() {
-        return functionToCall(twJoin.apply(null, arguments as any))
-    }
-}
-
-export function createTailwindMerge2(
-    createConfigFirst: CreateConfigFirst,
-    ...createConfigRest: CreateConfigSubsequent[]
-): TailwindMerge {
-    let configUtils: ConfigUtils
-    // let cacheGet: ConfigUtils['cache']['get']
-    let cacheSet: ConfigUtils['cache']['set']
-    let functionToCall = initTailwindMerge
-
-    function initTailwindMerge(classList: string) {
-        const config = createConfigRest.reduce(
-            (previousConfig, createConfigCurrent) => createConfigCurrent(previousConfig),
-            createConfigFirst() as GenericConfig,
-        )
-
-        configUtils = createConfigUtils(config)
-        // cacheGet = configUtils.cache.get
-        // cacheSet = configUtils.cache.set
-        functionToCall = tailwindMerge
-
-        return tailwindMerge(classList)
-    }
-
-    function tailwindMerge(classList: string) {
-        // const cachedResult = cacheGet(classList)
-
-        // if (cachedResult) {
-        //     return cachedResult
-        // }
-
-        const result = mergeClassList2(classList, configUtils)
-        // cacheSet(classList, result)
-
-        return result
-    }
-
-    return function callTailwindMerge() {
-        return functionToCall(twJoin.apply(null, arguments as any))
-    }
-}
-
-export function createTailwindMergeOriginal(
-    createConfigFirst: CreateConfigFirst,
-    ...createConfigRest: CreateConfigSubsequent[]
-): TailwindMerge {
-    let configUtils: ConfigUtils
-    // let cacheGet: ConfigUtils['cache']['get']
-    let cacheSet: ConfigUtils['cache']['set']
-    let functionToCall = initTailwindMerge
-
-    function initTailwindMerge(classList: string) {
-        const config = createConfigRest.reduce(
-            (previousConfig, createConfigCurrent) => createConfigCurrent(previousConfig),
-            createConfigFirst() as GenericConfig,
-        )
-
-        configUtils = createConfigUtils(config)
-        // cacheGet = configUtils.cache.get
-        // cacheSet = configUtils.cache.set
-        functionToCall = tailwindMerge
-
-        return tailwindMerge(classList)
-    }
-
-    function tailwindMerge(classList: string) {
-        // const cachedResult = cacheGet(classList)
-
-        // if (cachedResult) {
-        //     return cachedResult
-        // }
-
-        const result = mergeClassListOriginal(classList, configUtils)
         // cacheSet(classList, result)
 
         return result
--- tests/benchmark.bench.ts
+++ tests/benchmark.bench.ts
@@ -1,8 +1,43 @@
 import { Bench } from 'tinybench'
 
-import { getDefaultConfig } from '../src'
-import { createTailwindMergeBench } from '../src/lib/create-tailwind-merge'
+import { ClassNameValue, getDefaultConfig, twJoin } from '../src'
 import { mergeClassList, mergeClassList2, mergeClassListOriginal } from '../src/lib/merge-classlist'
+import { createConfigUtils } from '../src/lib/config-utils'
+import { GenericConfig } from '../src/lib/types'
+
+type CreateConfigFirst = () => GenericConfig
+type CreateConfigSubsequent = (config: GenericConfig) => GenericConfig
+type TailwindMerge = (...classLists: ClassNameValue[]) => string
+type ConfigUtils = ReturnType<typeof createConfigUtils>
+
+export function createTailwindMergeBench(
+    mergeClassList: (value: string, configUtils: ConfigUtils) => string,
+    createConfigFirst: CreateConfigFirst,
+    ...createConfigRest: CreateConfigSubsequent[]
+): TailwindMerge {
+    let configUtils: ConfigUtils
+    let functionToCall = initTailwindMerge
+
+    function initTailwindMerge(classList: string) {
+        const config = createConfigRest.reduce(
+            (previousConfig, createConfigCurrent) => createConfigCurrent(previousConfig),
+            createConfigFirst() as GenericConfig,
+        )
+
+        configUtils = createConfigUtils(config)
+        functionToCall = tailwindMerge
+
+        return tailwindMerge(classList)
+    }
+
+    function tailwindMerge(classList: string) {
+        return mergeClassList(classList, configUtils)
+    }
+
+    return function callTailwindMerge() {
+        return functionToCall(twJoin.apply(null, arguments as any))
+    }
+}
 
 const bench = new Bench({
     time: 1000,
